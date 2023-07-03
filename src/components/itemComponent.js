a877f73fc72f299a3cad4824bbdc8b70ccf64c66
ryevych <ryevych@gmail.com>
2023-07-03 06:43:32 +0300

    ncnf

--- src/components/itemComponent.js
+++ src/components/itemComponent.js
@@ -1,11 +1,22 @@
-import { View, Image, Text } from "react-native";
+import { View, Image, Text, StyleSheet } from "react-native";
 
-export default ItemComponent = function(item){
-    return (
-        <View>
-            <Image source={item.thumbnailUrl}/>
-            <Image source={item.iconlUrl}/>
-            <Text>{item.title}</Text>
-        </View>
-    )
-}
+export default ItemComponent = function ({ item }) {
+  return (
+    <View style={styles.container}>
+      <Image source={item.thumbnailUrl} />
+      <Image source={item.iconlUrl} />
+      <Text>{item.title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    backgroundColor: "#333",
+    // marginLeft: "2.5%",
+    // marginRight: "2.5%",
+    width: "50%",
+    // flex: 1,
+  },
+});
